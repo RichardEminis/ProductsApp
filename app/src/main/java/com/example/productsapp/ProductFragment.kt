3085db104062ab1389e66db7cc04ffe765b2cf0b
Alexandr Belyaev <eminis.ru@gmail.com>
2024-03-12 22:10:33 +0300

    PA-4 Add Button "Back to Menu"

--- app/src/main/java/com/example/productsapp/ProductFragment.kt
+++ app/src/main/java/com/example/productsapp/ProductFragment.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.ImageView
 import androidx.fragment.app.Fragment
 import com.example.productsapp.databinding.FragmentProductBinding
@@ -50,6 +51,12 @@ class ProductFragment : Fragment() {
 
         product?.let { product ->
             bind(product, view.findViewById(R.id.ivProductImage))
+        }
+
+        val backButton = view.findViewById<Button>(R.id.btnBackToList)
+
+        backButton.setOnClickListener {
+            requireActivity().onBackPressedDispatcher.onBackPressed()
         }
     }
 }
